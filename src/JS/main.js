03691e66d5068944868d28511119ba384fceaf1d
djswns7 <duftlagltkfwk@gmail.com>
2023-02-17 12:01:57 +0900

    리액트로 마크업 변경 전 세팅

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,35 +1,35 @@
-var body = document.querySelector("body");
+const body = document.querySelector("body")
 
-var MIN_DURATION = 5;
+const MIN_DURATION = 5
 
 function makeSnowflake() {
-  var snowflake = document.createElement("div");
-  var delay = Math.random() * 100;
-  var initialOpacity = Math.random() + 0.3;
+  const snowflake = document.createElement("div")
+  const delay = Math.random() * 100
+  const initialOpacity = Math.random() + 0.3
   // 애니메이션의 지속시간 == 속도
-  var duration = Math.random() * 20 + MIN_DURATION;
-  var flakeWidth = snowflake.style.width;
+  const duration = Math.random() * 20 + MIN_DURATION
+  const flakeWidth = snowflake.style.width
 
-  snowflake.classList.add("snowflake");
+  snowflake.classList.add("snowflake")
   // snowflake.style.left = `${Math.random()*window.screen.width}px`;
-  snowflake.style.left = Math.random() * 99 + "vw";
+  snowflake.style.left = `${Math.random() * 99}vw`
   // snowflake.style.left = `${calc(Math.random()*99vw - flakeWidthpx)}`;
-  snowflake.style.animationDelay = delay + "s";
-  snowflake.style.opacity = initialOpacity;
-  snowflake.style.animation = "snowFall " + duration + "s linear";
+  snowflake.style.animationDelay = `${delay}s`
+  snowflake.style.opacity = initialOpacity
+  snowflake.style.animation = `snowFall ${duration}s linear`
 
-  body.appendChild(snowflake);
+  body.appendChild(snowflake)
 
-  setTimeout(function () {
-    body.removeChild(snowflake);
+  setTimeout(() => {
+    body.removeChild(snowflake)
     // makeSnowflake(); // 눈송이 무한생성
-  }, (duration + delay) * 1000);
+  }, (duration + delay) * 1000)
 }
 
 function makeSnowflakes(n) {
-  for (var index = 0; index < n; index++) {
+  for (let index = 0; index < n; index++) {
     // makeSnowflake();
-    setTimeout(makeSnowflake, Math.random() * 20000);
+    setTimeout(makeSnowflake, Math.random() * 20000)
   }
 }
 
